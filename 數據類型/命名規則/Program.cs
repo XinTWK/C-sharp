651dcc7225a3385146ffbd0c7e3c4d3514c0520a
Justin Liu <37977002+XinTWK@users.noreply.github.com>
2021-07-17 17:25:47 +0800

    Update Program.cs

--- 數據類型/命名規則/Program.cs
+++ 數據類型/命名規則/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// https://www.dofactory.com/csharp-coding-standards  可以參考
+ https:www.dofactory.com/csharp-coding-standards  可以參考
 
 namespace 命名規則
 {   // class names like ClientActivity
