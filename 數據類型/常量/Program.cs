36924abd04bb044b4efe3fe9fd39d0dd4a4a4392
XinTWK <fly4742@gmail.com>
2021-07-17 19:27:13 +0800

    上傳

---
+++ 數據類型/常量/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 常量
+{
+    // Constants are immutable values which are known 
+    // at compile time and do not change for the life of the program.
+    // 常量是不可變得值,他們在編譯時已知並且在程序的生命週期內不會改變
+    
+
+    class Program
+    {
+        // constants as fields
+        const double PI = 3.14159265359;
+
+
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
