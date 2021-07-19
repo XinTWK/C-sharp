59896c9108cc68b17aa063f1aa350c77096278ac
XinTWK <fly4742@gmail.com>
2021-07-19 16:31:31 +0800

    上傳

---
+++ 成員/Members.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 成員
+{
+    class Members
+    {
+        // member - private field
+        private string memberName;
+        private string jobTitle;
+        private int salary;
+
+        // member - public field
+        public int age;
+
+        // member - property - exposes jobTitle safely - properties start with a captial letter
+        public string JubTitle
+        {
+            get
+            {
+                return jobTitle;
+            }
+            set
+            {
+                jobTitle = value;
+            }
+        }
+
+        // public member Method - can be called from other classes
+        public void Introducing(bool isFriend)
+        {
+            if(isFriend)
+            {
+                SharingPrivateInfo();
+            }
+            else
+            {
+                Console.WriteLine("Hi, my name is {0}, and my job title is {1}. I'm {2} years old",
+                   memberName,jobTitle,age );
+            }
+        }
+
+        private void SharingPrivateInfo()
+        {
+            Console.WriteLine("My salary is {0}", salary);
+        }
+
+        // member constructor 構造函數
+        public Members()
+        {
+            age = 30;
+            memberName = "Lucy";
+            salary = 60000;
+            jobTitle = "Deceloper";
+            Console.WriteLine("Object created");
+        }
+
+
+        // member - finalizer - destructor 析構函數
+        ~Members()
+        {
+            // cleanup statements
+            Console.WriteLine("Deconstruction of Members object");
+            Debug.Write("Deconstruction of Members object");
+        }
+
+    }
+
+}
---
+++ 成員/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 成員
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Members member1 = new Members();
+            member1.Introducing(true);
+            Console.Read();
+        }
+
+    }
+}
