79e5ddd2dae4e383acb1c2dab3a8fdea1d53c851
XinTWK <fly4742@gmail.com>
2021-07-19 16:26:41 +0800

    上傳

---
+++ 屬性/Box.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 屬性
+{
+    class Box
+    {
+        // member variable
+        private int length;
+        private int height;
+        //public int width;
+        private int volume;
+
+        public int Width { get; set; } //  程式瑪片段: prop 兩下tab
+
+        //public int Width
+        //{
+        //    get
+        //    {
+        //        return this.width;
+        //    }
+        //    set
+        //    {
+        //        this.width = value;
+        //    }
+        //}
+
+        public int Volume
+        {
+            get
+            {
+                return this.length * this.height * this.Width;
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    height = -value;
+                }
+                else
+                {
+                    height = value;
+                }
+            }
+        }
+
+        // challenge: create a read only property "FrontSurface" which calculates the
+        // front surface based on height and length
+
+        public int FrontSurface
+        {
+            get
+            {
+                return height * length;
+            }
+        }
+
+
+        public Box(int length, int height, int width)
+        {
+            this.length = length;
+            this.height = height;
+            this.Width = width;
+        }
+
+
+
+        public void SetLength(int length)
+        {
+            if (length < 0)
+            {
+                throw new Exception("Length should be higher than 0");
+            }
+            this.length = length;
+        }
+
+        public int GetLength()
+        {
+            return this.length;
+        }
+
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Length is {0} and height is {1} and width is {2} so the volume is {3}",
+             length, height, Width, volume = length * height * Width);
+
+        }
+
+    }
+}
---
+++ 屬性/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 屬性
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box(3, 4, 5);
+            //Box box = new Box();
+            //box.length = 3;
+            //box.SetLength(4);
+            //box.height = 4;
+            //box.Height = -4;
+            //box.Width = 5;
+            Console.WriteLine("Box Width is " + box.Width);
+            box.Width = 10;
+            Console.WriteLine("Box Width is " + box.Width);
+
+            Console.WriteLine("Box volume is " + box.Volume);
+            box.DisplayInfo();
+
+            Console.WriteLine("Front surface of the box is {0}" , box.FrontSurface);
+            Console.Read();
+        }
+
+    }
+}
