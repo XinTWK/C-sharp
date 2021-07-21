3459b1962b90f580fa6773658f33dd786ad4ce7f
XinTWK <fly4742@gmail.com>
2021-07-21 23:41:08 +0800

    上傳

---
+++ Arrays/ArraylistC/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaring an ArrayList with undefined amount of object
+            ArrayList myArrayList = new ArrayList();
+            // declaring an ArrayList with defined amount of object 
+            ArrayList myArrayList2 = new ArrayList(100);
+
+            myArrayList.Add(25);
+            myArrayList.Add("Hello");
+            myArrayList.Add(13.36);
+            myArrayList.Add(13);
+            myArrayList.Add(129);
+            myArrayList.Add(119.3);
+            myArrayList.Add(13);
+
+            // delete element with specific value from the arraylist
+            myArrayList.Remove(13);
+
+            // delete element at specific position
+            myArrayList.RemoveAt(0);
+
+            Console.WriteLine(myArrayList.Count);
+
+            foreach(object obj in myArrayList)
+            {
+
+            }
+            
+        }
+    }
+}
--- Arrays/ParamsExample/Program.cs
+++ Arrays/ParamsExample/Program.cs
@@ -10,8 +10,43 @@ namespace ParamsExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Price is {0}, PI is {1}, at is {2}", 32, 3.14, '@');
-            Console.WriteLine("{0}+{1}+{2}+{3}+{4}+{5}+{6}+{7}={8}", 1, 2, 3, 4, 5, 6, 7, 8, 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8);
+            int price = 50;
+            float pi = 3.14f;
+            char at = '@';
+            string book = "The Hobbit";
+
+            ParamsMethod2(price,pi,at,book);
+            ParamsMethod2("Hello",5.3,'$');
+
+            //Console.WriteLine("Price is {0}, PI is {1}, at is {2}", 32, 3.14, '@');
+            //Console.WriteLine("{0}+{1}+{2}+{3}+{4}+{5}+{6}+{7}={8}", 1, 2, 3, 4, 5, 6, 7, 8, 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8);
+
+            ParamsMethod(); // 輸出空的 程式也不會出錯
+
+            //ParamsMethod("This", " is ", " a ", " long ", " string ", " I ", " have ", " no ", " idea ", " when ", " it's ", " going ", " to ", " end ", "....."); // I can keep going
+
+            Console.ReadKey();
+
         }
+
+        public static void ParamsMethod(params string[] sentence)
+        {
+            for(int i = 0; i< sentence.Length; i++)
+            {
+                Console.Write(sentence[i]);
+            }
+        }
+
+        public static void ParamsMethod2(params object[] stuff)
+        {
+            // foreach loop to go through the array of objects
+            foreach (object obj in stuff)
+            {
+                // print each object followed by a space
+                Console.WriteLine(obj + " ");
+            }
+            Console.WriteLine();
+        }
+
     }
 }
