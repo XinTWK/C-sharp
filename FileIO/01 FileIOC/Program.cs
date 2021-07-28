132b293290eead056b6c1331c52f4a226ed56f90
XinTWK <fly4742@gmail.com>
2021-07-28 23:33:35 +0800

    上傳

---
+++ FileIO/01 FileIOC/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_FileIOC
+{
+    class Program
+    {
+        // Exanple 1 - reading Text
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"D:\C#\FileIO\01 FileIOC\textFile.txt");
+            Console.WriteLine("TextFile contaions following text: \n{0}",text);
+
+            string[] lines = System.IO.File.ReadAllLines(@"D:\C#\FileIO\01 FileIOC\textFile.txt");
+            Console.WriteLine("Contents of textfile.txt = ");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Polymorphism/01 PolymrophismC/M3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_PolymrophismC
+{
+    class M3 : BMW
+    {
+        public M3(int hp, string color, string model) : base(hp, color,model)
+        {
+            this.Model = model;
+        }
+
+
+        //public override void Repair()
+        //{
+        //    base.Repair();  
+        //}
+    }
+}
