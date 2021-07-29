61046ec089bee54789f07b2da5bd85f0fb69f293
XinTWK <fly4742@gmail.com>
2021-07-29 15:18:20 +0800

    上傳

--- FileIO/01 FileIOC/Program.cs
+++ FileIO/01 FileIOC/Program.cs
@@ -1,22 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace _01_FileIOC
 {
     class Program
     {
-        // Exanple 1 - reading Text
+
         static void Main(string[] args)
         {
-            string text = System.IO.File.ReadAllText(@"D:\C#\FileIO\01 FileIOC\textFile.txt");
-            Console.WriteLine("TextFile contaions following text: \n{0}",text);
+            // Method 1
+            string[] lines = { "First 250", "Second 242", "Third 240" };
 
-            string[] lines = System.IO.File.ReadAllLines(@"D:\C#\FileIO\01 FileIOC\textFile.txt");
-            Console.WriteLine("Contents of textfile.txt = ");
+            File.WriteAllLines(@"D:\C#\FileIO\01 FileIOC\highscores.txt", lines);
 
+            //// Method 2
+            //Console.WriteLine("Please give the file a name");
+            //string fileName = Console.ReadLine();
+            //Console.WriteLine("Please enter the text for the file");
+            //string input = Console.ReadLine();
+            //File.WriteAllText(@"D:\C#\FileIO\01 FileIOC\" + fileName + ".txt", input);
+
+            // Method 3
+            using (StreamWriter file = new StreamWriter(@"D:\C#\FileIO\01 FileIOC\myText2.txt"))
+            {
+                foreach (string line in lines)
+                {
+                    if(line.Contains("2"))
+                    {
+                        file.WriteLine(line);
+                    }
+                }
+            }
+
+            using(StreamWriter file = new StreamWriter(@"D:\C#\FileIO\01 FileIOC\myText2.txt", true))
+            {
+                file.WriteLine("Additional line");
+            }
+
+            // Exanple 1 - reading Text
+            //string text = System.IO.File.ReadAllText(@"D:\C#\FileIO\01 FileIOC\textFile.txt");
+            //Console.WriteLine("TextFile contaions following text: {0}", text);
+
+            //string[] lines = System.IO.File.ReadAllLines(@"D:\C#\FileIO\01 FileIOC\textFile.txt");
+            //Console.WriteLine("Contents of textfile.txt = ");
+            //foreach (string line in lines)
+            //{
+            //    Console.WriteLine("\t" + line);
+            //}
 
             Console.ReadKey();
         }
