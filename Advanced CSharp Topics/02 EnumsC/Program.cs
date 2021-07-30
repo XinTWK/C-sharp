d972a0bbb38f2749c011f053b09e24b01a7ab9ff
XinTWK <fly4742@gmail.com>
2021-07-30 16:45:28 +0800

    上傳

---
+++ Advanced CSharp Topics/02 EnumsC/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_EnumsC
+{
+    enum Day { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
+    enum Month { January = 1, February, March, April, May, June, July = 12, August, September, October, November, December }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day friday = Day.Friday;
+            Day sunday = Day.Sunday;
+
+            Day a = Day.Friday;
+
+            Console.WriteLine(friday == a);
+
+            Console.WriteLine(Day.Monday);
+            Console.WriteLine((int)Day.Monday);
+
+            Console.WriteLine((int)Month.February);
+            Console.WriteLine((int)Month.August);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Advanced CSharp Topics/04 RandomT/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_RandomT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Random dice = new Random();
+            //int numEyes;
+
+            //for(int i = 0; i < 10; i++)
+            //{
+            //    numEyes = dice.Next(1,7);
+            //    Console.WriteLine(numEyes);
+            //}
+
+            Random yesNoMaybe = new Random();
+            Console.WriteLine("Please enter your question");
+            Console.ReadLine();
+
+            int answerNum;
+            answerNum = yesNoMaybe.Next(1, 4);
+            
+            if(answerNum == 1)
+            {
+                Console.WriteLine("Yes");
+            }else if (answerNum == 2)
+            {
+                Console.WriteLine("Maybe");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
