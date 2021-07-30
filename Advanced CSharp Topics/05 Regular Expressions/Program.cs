fe5fcd731d71ccf8a0e5cae15b5c750e6ce39159
XinTWK <fly4742@gmail.com>
2021-07-30 22:13:26 +0800

    上傳

--- Advanced CSharp Topics/05 Regular Expressions/Program.cs
+++ Advanced CSharp Topics/05 Regular Expressions/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace _05_Regular_Expressions
@@ -10,7 +11,22 @@ namespace _05_Regular_Expressions
     {
         static void Main(string[] args)
         {
+            string pattern = @"there";
+            Regex regex = new Regex(pattern);
 
+            string text = "Hi there, my number is 12314";
+
+            MatchCollection matchCollection = regex.Matches(text);
+
+            Console.WriteLine("{0} hits found:\n {1}",matchCollection.Count, text);
+
+            foreach(Match hit in matchCollection)
+            {
+                GroupCollection group = hit.Groups;
+                Console.WriteLine("{0} found at {1}",group[0].Value,group[0].Index);
+            }
+
+            Console.ReadKey();
         }
     }
 }
