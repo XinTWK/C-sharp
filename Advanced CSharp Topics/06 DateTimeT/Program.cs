4fb762f4608141aa8dfbcf83bb3bd7804b0f4bdb
XinTWK <fly4742@gmail.com>
2021-07-31 23:05:30 +0800

    上傳

---
+++ Advanced CSharp Topics/06 DateTimeT/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_DateTimeT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime(2021, 7, 31);
+
+            Console.WriteLine("My birthday is {0}",dateTime);
+        }
+    }
+}
