6cc19201194bb36a42a42fbc0b862c98ae461ff1
XinTWK <fly4742@gmail.com>
2021-08-02 16:52:07 +0800

    Update Program.cs

--- Advanced CSharp Topics/08 MainArgsDemo/Program.cs
+++ Advanced CSharp Topics/08 MainArgsDemo/Program.cs
@@ -36,7 +36,55 @@ namespace _08_MainArgsDemo
                 return;
             }
 
-            Console.WriteLine("Hello " + args[0]);
+            // check the length of args
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Invalid arguments, please use the help command for instructions");
+                // pause
+                Console.ReadKey();
+                // quit the app
+                return;
+            }
+
+            // In C# 6 and earlier, you must declare a variable in a separate statement before you pass it as an out argument
+            // since if the parsing operation fails the variable passed as out will have it's default value if it was a value
+            // if it was a value type, or null if it's a reference type
+            // it is more logical to declare and pass the variable in the same line
+            bool isNum1Parsed = float.TryParse(args[1], out float num1);
+            bool isNum2Parsed = float.TryParse(args[2], out float num2);
+
+            if (!isNum1Parsed || !isNum2Parsed)
+            {
+                Console.WriteLine("Invalid arguments, please use the help command for instructions");
+                // pause
+                Console.ReadKey();
+                // quite the app
+                return;
+            }
+
+            // a variable to store the results
+            float result;
+
+            switch(args[0])
+            {
+                // case 1 'add' add the two numbers and print the value
+                case "add":
+                    result = num1 + num2;
+                    Console.WriteLine($"The sum of {num1} and {num2} is {result}");
+                    break;
+
+                // case 2 'sub' sub the two numbers and print the valuer
+                case "sub":
+                    result = num1 - num2;
+                    Console.WriteLine($"The sub of {num1} and {num2} is {result}");
+                    break;
+
+                // default case, here we handle wrong commands by printing an error message
+                default:
+                    Console.WriteLine("Invalid arguments, please use the help command for instructions");
+                    break;
+            }
+
             Console.ReadKey();
 
         }
