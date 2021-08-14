15aad4b94f66b753eb2d2508d9113767c78cd059
XinTWK <fly4742@gmail.com>
2021-08-14 21:20:58 +0800

    上傳

--- Linq1/02 LINQToObjectsAndQueryOperators/Program.cs
+++ Linq1/02 LINQToObjectsAndQueryOperators/Program.cs
@@ -25,6 +25,16 @@ namespace _02_LINQToObjectsAndQueryOperators
 
     }
 
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+
+
+    }
 
 
 }
